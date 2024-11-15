2a7b38fec6ea91cc90290ec4f9fad8449e91b60e
Ali Hassan Zahid <162284502+ALIHASSAN0090@users.noreply.github.com>
2024-11-15 10:11:14 +0500

    linked list add node and print node method

--- Linked-List/Singly_linked_list/main.go
+++ Linked-List/Singly_linked_list/main.go
@@ -1,5 +1,45 @@
 package main
 
+import "fmt"
+
+type Node struct {
+	data int
+	next *Node
+}
+
+type LinkedList struct {
+	head *Node
+}
+
 func main() {
 
+	list := &LinkedList{}
+	list.AddNode(10)
+	list.AddNode(20)
+	list.AddNode(30)
+
+	list.PrintList()
+}
+
+func (list *LinkedList) PrintList() {
+	current := list.head
+	for current != nil {
+		fmt.Println(current.data)
+		current = current.next
+	}
+
+}
+
+func (list *LinkedList) AddNode(data int) {
+	newNode := &Node{data: data}
+
+	if list.head == nil {
+		list.head = newNode
+	} else {
+		current := list.head
+		for current.next != nil {
+			current = current.next
+		}
+		current.next = newNode
+	}
 }
