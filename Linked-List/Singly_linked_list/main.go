b6a42f20aeb6b9d21a2343bb43d397d57cf20034
Ali Hassan Zahid <162284502+ALIHASSAN0090@users.noreply.github.com>
2024-11-15 10:40:51 +0500

    add to specific index method

--- Linked-List/Singly_linked_list/main.go
+++ Linked-List/Singly_linked_list/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type Node struct {
 	data int
@@ -18,6 +20,16 @@ func main() {
 	list.AddNode(20)
 	list.AddNode(30)
 
+	fmt.Printf("Initial list:\n")
+	list.PrintList()
+	list.AddNodeToFront(30)
+	fmt.Printf("List after adding 30 to the front:\n")
+	list.PrintList()
+	list.AddNode(70)
+	fmt.Printf("List after adding 70 to the front:\n")
+	list.PrintList()
+	list.AddtoIndex(3, 90)
+	fmt.Printf("List after adding 90 at index 3:\n")
 	list.PrintList()
 }
 
@@ -42,4 +54,36 @@ func (list *LinkedList) AddNode(data int) {
 		}
 		current.next = newNode
 	}
+}
+
+func (list *LinkedList) AddNodeToFront(data int) {
+	newNode := &Node{data: data}
+	newNode.next = list.head
+	list.head = newNode
+}
+
+func (list *LinkedList) AddtoIndex(index, data int) {
+
+	if index < 0 {
+		fmt.Print("invalid index")
+	}
+
+	if index == 0 {
+		list.AddNodeToFront(data)
+	}
+
+	newNode := &Node{data: data}
+	current := list.head
+
+	for i := 0; i < index-1 && current != nil; i++ {
+		current = current.next
+	}
+
+	if current == nil {
+		fmt.Print("index out of bounds")
+		return
+	}
+	newNode.next = current.next
+	current.next = newNode
+
 }
